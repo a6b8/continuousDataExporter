f2f83bbc706de081a8aeccdd34afdea8a6586b15
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2024-03-11 19:20:48 +0100

    Add tests #4

--- test.mjs
+++
@@ -1,7 +0,0 @@
-const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-
-const size = 3
-const result = arr.slice( 0, size)
-arr.splice( 0, size )
-console.log( 'process', result )
-console.log( '>>>', arr )
